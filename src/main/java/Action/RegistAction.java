564a84f6c7495a127433873e8a66c81a54a18037
ThreadDao <zongyufen@foxmail.com>
2017-07-06 10:27:40 +0800

    temp

--- src/main/java/Action/RegistAction.java
+++ src/main/java/Action/RegistAction.java
@@ -1,6 +1,11 @@
 package Action;
 
+import Bean.User;
+import Dao.UserDaoImpl;
 import com.opensymphony.xwork2.ActionSupport;
+
+import javax.annotation.Resource;
+import javax.swing.*;
 
 /**
  * Created by SSS on 2017/7/6.
@@ -14,4 +19,35 @@ public class RegistAction extends ActionSupport {
     *        ERROR means:login error
     * mark need return messages
     * */
+    private static final long serialVersionUID = 5386429367683022172L;
+
+    @Resource
+    private UserDaoImpl userDao;
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    @Override
+    public String execute() throws Exception {
+        return super.execute();
+    }
+    //用户注册
+    public String regist(){
+        if(null==userDao)
+            userDao=new UserDaoImpl();
+        int result=userDao.addUser(user.getUname(),user.getPassword(),user.getPhoto());
+        System.out.println(result);
+        if(result!=-1){
+            JOptionPane.showMessageDialog(null,"注册成功！");
+            return SUCCESS;
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"注册失败！");
+            return ERROR;
+        }
+    }
 }
