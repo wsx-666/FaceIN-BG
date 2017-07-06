d2716a89bfc74749ffa92434a528c2563c13d656
CatchCake <chinaqqyx@qq.com>
2017-07-06 15:54:47 +0900

    完成FACE++人脸比较，通过/Utils/FaceRequestUtil.java 调用

---
+++ src/main/java/Utils/Constant.java
@@ -0,0 +1,14 @@
+package Utils;
+
+/**
+ * Created by Dao on 2017/7/6.
+ */
+public class Constant {
+    /*
+    * FacePlusPlus face API
+    * */
+    public static final String faceppURL="https://api-cn.faceplusplus.com/facepp/v3/compare";
+    public static final String faceppAPIKey="aS4bbUHmbdnbIWxWfVQ0mlCKyqDrz2cK";
+    public static final String faceppAPISecret="-_7oUGEjIx3hp40Y-O2GLnSpjfkM3WOk";
+    public static final float confidence=85;
+}
